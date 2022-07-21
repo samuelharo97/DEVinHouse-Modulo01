8c845e275d8d0e66178f13b7b30e50d898109a84
samuelharo97 <samuel.hm@hotmail.com>
2022-07-21 14:26:12 -0300

    Exercicio 06 - Funcionando

--- M1S04 - Roteiro de Exercicios/M1S04 - Projeto/script.js
+++ M1S04 - Roteiro de Exercicios/M1S04 - Projeto/script.js
@@ -1,5 +1,3 @@
-let accounts = document.querySelector('#account')
-
 const contasClientes = [
   {
     id: 1,
@@ -18,36 +16,56 @@ const contasClientes = [
   }
 ]
 
+let accounts = document.querySelector('#account')
+let sendButton = document.querySelector('#send')
+let action = document.querySelector('#options')
+let amount = document.querySelector('#amount')
+let selectedOption
+
+accounts.addEventListener('change', function () {
+  selectedOption = Number(accounts.value)
+  console.log(typeof selectedOption)
+})
+
+
+sendButton.addEventListener('click', function () {
+  if (action.value === 'withdraw') {
+    withdrawMoney(amount.value, selectedOption)
+  } else if (action.value === 'deposit') {
+    depositMoney(amount.value, selectedOption)
+  }
+})
+
+
 function addAccount() {
-  contasClientes.forEach((n, i) => {
-    accounts.innerHTML += `<option class="conta${i}"> ${contasClientes[i].nome} </option>`
+  contasClientes.forEach((object, i) => {
+    accounts.innerHTML += `<option value="${i + 1}"> ${
+      contasClientes[i].nome
+    } </option>`
   })
 }
 
-function withdrawMoney(value, idDaConta) {
-  let selectedAccount = contasClientes.find(
-    conta => conta.id === idDaConta
-  ).saldo
 
-  if (value > selectedAccount) {
+function withdrawMoney(value, idDaConta) {
+  selectedAccount = contasClientes.find(conta => conta.id === idDaConta).saldo
+  if (value > selectedAccount || value <= 0) {
     alert('Valor inválido.')
   } else {
-    selectedAccount -= value
+    selectedAccount = Number(selectedAccount) - Number(value)
     contasClientes[idDaConta - 1].saldo = selectedAccount
     alert(`Saque concluído. Saldo atual: R$ ${selectedAccount}`)
   }
 }
 
 function depositMoney(value, idDaConta) {
-  let selectedAccount = contasClientes.find(
-    conta => conta.id === idDaConta
-  ).saldo
-
+  selectedAccount = contasClientes.find(conta => conta.id === idDaConta).saldo
   if (value <= 0) {
     alert('Valor inválido.')
   } else {
-    selectedAccount += value
+    selectedAccount = Number(selectedAccount) + Number(value)
     contasClientes[idDaConta - 1].saldo = selectedAccount
     alert(`Depósito concluído. Saldo atual: R$ ${selectedAccount}`)
   }
 }
+
+window.onload = addAccount()
