20b87199c40fc2a8ba507eed1d4b8785e39de84e
samuelharo97 <samuel.hm@hotmail.com>
2022-07-26 13:34:57 -0300

    Exercicio 04

--- M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-04/index.js
+++ M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-04/index.js
@@ -1,8 +1,18 @@
-class product {
+class Produto {
   constructor(nome, preco, emEstoque, quantidade) {
     this.nome = nome
     this.preco = preco
     this.emEstoque = emEstoque
     this.quantidade = quantidade
+  }
+}
+
+class Pedido {
+  constructor(numeroPedido, dataPedido, estaPago, listaProdutos, nomeCliente) {
+    this.numeroPedido = numeroPedido
+    this.dataPedido = new Date().toLocaleDateString()
+    this.estaPago = false
+    this.listaProdutos = []
+    this.nomeCliente = nomeCliente
   }
 }
