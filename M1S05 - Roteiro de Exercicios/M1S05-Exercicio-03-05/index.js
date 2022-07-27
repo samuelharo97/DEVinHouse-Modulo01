98c8861d7fc52acbefc824801fa0f3f2a7b96bc8
samuelharo97 <samuel.hm@hotmail.com>
2022-07-26 22:43:06 -0300

    ex 06 fixed test

--- M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-05/index.js
+++ M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-05/index.js
@@ -23,12 +23,12 @@ class Pedido {
   }
 
   calcularTotal() {
-    for (let i = 0; i < this.listaProdutos.length, i++){
-       let total = 0
-       const price = this.listaProdutos[i].pedido.preco
-       const quantity = this.listaProdutos[i].pedido.quantidade
-       total = price * quantity
-       this.listaProdutos[i].push(total)
+    for (let i = 0; i < this.listaProdutos.length, i++; ) {
+      let total = 0
+      const price = this.listaProdutos[i].pedido.preco
+      const quantity = this.listaProdutos[i].pedido.quantidade
+      total = price * quantity
+      this.listaProdutos[i].push(total)
     }
   }
 }
