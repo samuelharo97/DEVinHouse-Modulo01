3169800f221a5cc7c17422ac693c787fa5ef6dad
samuelharo97 <samuel.hm@hotmail.com>
2022-07-26 22:59:34 -0300

    exercicio 07 working

--- M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-05/index.js
+++ M1S05 - Roteiro de Exercicios/M1S05-Exercicio-03-05/index.js
@@ -32,3 +32,21 @@ class Pedido {
     }
   }
 }
+
+const produto1 = new Produto('Chocolate', 5, true, 10)
+const produto2 = new Produto('Celular', 2000, true, 1)
+const produto3 = new Produto('Cerveja', 35, true, 3)
+const produto4 = new Produto('Teclado', 200, true, 1)
+const produto5 = new Produto('Mouse', 150, true, 1)
+
+const pedido1 = new Pedido(20220001, 'Samuel Haro')
+const pedido2 = new Pedido(20220002, 'Gabrielly Pontes')
+
+pedido1.adicionarProduto(produto1)
+pedido1.adicionarProduto(produto4)
+pedido1.adicionarProduto(produto5)
+
+pedido2.adicionarProduto(produto2)
+pedido2.adicionarProduto(produto3)
+
+
