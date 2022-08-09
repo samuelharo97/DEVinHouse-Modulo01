6658a60130755b9bf9176e66d24bfb5e610b2c86
samuelharo97 <samuel.hm@hotmail.com>
2022-08-09 18:28:26 -0300

    commit 3

--- M1S07 - Roteiro de Exercicios/Restaurante/src/components/Header.jsx
+++
@@ -1,5 +0,0 @@
-export function Header(){
-  return(
-    <h1>Pastel do Haro</h1>
-  )
-}
---
+++ M1S07 - Roteiro de Exercicios/Restaurante/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export function Header(){
+  return(
+    <div className="logo">
+      <img src="" alt="logo da pastelaria" />
+    <h1>Pastel do Haro</h1>
+    </div>
+  )
+}
