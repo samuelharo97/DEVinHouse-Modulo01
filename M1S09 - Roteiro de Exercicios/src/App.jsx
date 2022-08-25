ea5edd38f13d9ed59174c6169610c7ecd20c3a66
samuelharo97 <samuel.hm@hotmail.com>
2022-08-25 10:51:57 -0300

    ex 02

--- M1S09 - Roteiro de Exercicios/src/App.jsx
+++ M1S09 - Roteiro de Exercicios/src/App.jsx
@@ -1,60 +1,65 @@
-import { Footer, Header, Secao, FiltroSecao } from '@components';
-import { produtos } from '@services';
-import { useState } from 'react';
-import styles from './App.module.css';
+import { Footer, Header, Secao, FiltroSecao } from '@components'
+import { produtos } from '@services'
+import { ProdutosSelecionadosProvider } from 'contexts/ProdutosSelecionados'
+import { useState } from 'react'
+import styles from './App.module.css'
 
 function App() {
-  const [filtro, setFiltro] = useState(null);
+  const [filtro, setFiltro] = useState(null)
 
-  const secoes = Array.from(new Set(produtos.map((prod) => prod.secao)));
+  const secoes = Array.from(new Set(produtos.map(prod => prod.secao)))
 
-  const obterProdutosSecao = (secao) => {
-    return produtos.filter((p) => p.secao === secao);
-  };
+  const obterProdutosSecao = secao => {
+    return produtos.filter(p => p.secao === secao)
+  }
 
-  const obterSubSecoes = (secao) => {
-    const produtosComSubSecoes = obterProdutosSecao(secao).filter((p) => p.subSecao);
+  const obterSubSecoes = secao => {
+    const produtosComSubSecoes = obterProdutosSecao(secao).filter(
+      p => p.subSecao
+    )
 
-    return Array.from(new Set(produtosComSubSecoes.map((p) => p.subSecao)));
-  };
+    return Array.from(new Set(produtosComSubSecoes.map(p => p.subSecao)))
+  }
 
   const obterSecoesFiltradas = () => {
     if (filtro) {
-      return secoes.filter((s) => s === filtro);
+      return secoes.filter(s => s === filtro)
     }
-    return secoes;
-  };
+    return secoes
+  }
 
-  const handleSelecionarSecao = (secao) => {
+  const handleSelecionarSecao = secao => {
     if (secao === filtro) {
-      setFiltro(null);
-      return;
+      setFiltro(null)
+      return
     }
-    setFiltro(secao);
-  };
+    setFiltro(secao)
+  }
 
   return (
     <div className={styles.app}>
-      <Header />
-      <main className={styles.main}>
-        <FiltroSecao
-          secoes={secoes}
-          secaoSelecionada={filtro}
-          onSelecionar={handleSelecionarSecao}
-        />
-
-        {obterSecoesFiltradas().map((secao) => (
-          <Secao
-            key={secao}
-            nome={secao}
-            produtos={obterProdutosSecao(secao)}
-            subSecoes={obterSubSecoes(secao)}
+      <ProdutosSelecionadosProvider>
+        <Header />
+        <main className={styles.main}>
+          <FiltroSecao
+            secoes={secoes}
+            secaoSelecionada={filtro}
+            onSelecionar={handleSelecionarSecao}
           />
-        ))}
-      </main>
-      <Footer />
+
+          {obterSecoesFiltradas().map(secao => (
+            <Secao
+              key={secao}
+              nome={secao}
+              produtos={obterProdutosSecao(secao)}
+              subSecoes={obterSubSecoes(secao)}
+            />
+          ))}
+        </main>
+        <Footer />
+      </ProdutosSelecionadosProvider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
