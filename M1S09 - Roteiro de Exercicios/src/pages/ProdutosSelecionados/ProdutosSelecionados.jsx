3b7af40ce725649d7b588b3ab9c2eed1aec60088
samuelharo97 <samuel.hm@hotmail.com>
2022-08-27 13:50:00 -0300

    EX 08

--- M1S09 - Roteiro de Exercicios/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ M1S09 - Roteiro de Exercicios/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -1,7 +1,12 @@
+import { ListaCards } from '@components'
+import { useProdutosSelecionados } from '@contexts'
+
 export const ProdutosSelecionados = () => {
+  const { produtosSelecionados } = useProdutosSelecionados()
   return (
     <>
       <h1>Produtos Selecionados</h1>
+      <ListaCards produtos={produtosSelecionados} />
     </>
   )
 }
