aa33dfd712d2e7ac71659a8a080cf7574e789d8f
samuelharo97 <samuel.hm@hotmail.com>
2022-08-27 14:37:23 -0300

    fix lint issue

--- M1S09 - Roteiro de Exercicios/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ M1S09 - Roteiro de Exercicios/src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -1,11 +1,17 @@
 import { ListaCards } from '@components'
 import { useProdutosSelecionados } from '@contexts'
+import { produtos } from '@services'
 
 export const ProdutosSelecionados = () => {
   const { produtosSelecionados } = useProdutosSelecionados()
+  const total = produtosSelecionados.reduce((soma, item) => {
+    return soma + item.valor
+  }, 0)
+  
   return (
     <>
       <h1>Produtos Selecionados</h1>
+      <h2>{total}</h2>
       <ListaCards produtos={produtosSelecionados} />
     </>
   )
