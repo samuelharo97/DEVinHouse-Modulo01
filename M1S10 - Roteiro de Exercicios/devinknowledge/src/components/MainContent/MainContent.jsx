0a89b92c1c2d6107831816b5b5d9ea83d3c3c8cb
samuelharo97 <samuel.hm@hotmail.com>
2022-09-02 22:26:46 -0300

    ex 08

--- M1S10 - Roteiro de Exercicios/devinknowledge/src/components/MainContent/MainContent.jsx
+++ M1S10 - Roteiro de Exercicios/devinknowledge/src/components/MainContent/MainContent.jsx
@@ -18,38 +18,6 @@ export const MainContent = () => {
           language="FrontEnd"
           video="https://www.youtube.com/watch?v=x-4z_u8LcGc"
         />
-        <TipCard
-          key={2}
-          title="Dica dois"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti odit ut at unde officia vel obcaecati iusto modi officiis rem eius voluptatem nulla veritatis dolorum, consequatur labore id hic?"
-          category=""
-          language=""
-          video=""
-        />
-        <TipCard
-          key={3}
-          title="Dica tres"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti odit ut at unde officia vel obcaecati iusto modi officiis rem eius voluptatem nulla veritatis dolorum, consequatur labore id hic?"
-          category=""
-          language=""
-          video=""
-        />
-        <TipCard
-          key={4}
-          title="Dica quatro"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti odit ut at unde officia vel obcaecati iusto modi officiis rem eius voluptatem nulla veritatis dolorum, consequatur labore id hic?"
-          category=""
-          language=""
-          video=""
-        />
-        <TipCard
-          key={5}
-          title="Dica cinco"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam deleniti odit ut at unde officia vel obcaecati iusto modi officiis rem eius voluptatem nulla veritatis dolorum, consequatur labore id hic?"
-          category=""
-          language=""
-          video=""
-        />
       </CardList>
     </section>
   )
--- M1S10 - Roteiro de Exercicios/devinknowledge/src/context/AppContextProvider.jsx
+++ M1S10 - Roteiro de Exercicios/devinknowledge/src/context/AppContextProvider.jsx
@@ -1,8 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { AppContext } from './app-context'
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 export const AppContextProvider = ({ children }) => {
+  const [allTips, setAllTips] = useState([])
+  const [filter, setFilter] = useState(null)
+  const createTip = data => {
+    setAllTips(prevData => [...prevData, data])
+  }
+  const filterTip = query => {
+    if (query) {
+      setFilter(query)
+    } else {
+      setFilter(null)
+    }
+  }
+
+  const tips = filter
+    ? allTips.filter(tip => tip.titulo.includes(filter))
+    : allTips
+
   return (
-    <AppContext.Provider value={'conectado'}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ createTip, filterTip, tips }}>
+      {children}
+    </AppContext.Provider>
   )
 }
 
