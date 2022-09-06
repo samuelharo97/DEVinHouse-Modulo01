b518c745bfa046bf5431edd7539452aeb4b0c84b
samuelharo97 <samuel.hm@hotmail.com>
2022-09-06 15:59:04 -0300

    ex 1-2

---
+++ M1S11 - Roteiro de Exercicios/profiles/src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a, button {
+    text-decoration: none;
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  a:hover, button:hover {
+    filter: brightness(0.9);
+  }
+`
