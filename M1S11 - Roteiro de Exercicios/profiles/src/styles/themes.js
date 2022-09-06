92d22c8a83514b8ec451f641a3717240f0dd94e7
samuelharo97 <samuel.hm@hotmail.com>
2022-09-06 16:01:52 -0300

    fix ex 2

--- M1S11 - Roteiro de Exercicios/profiles/src/styles/themes.js
+++ M1S11 - Roteiro de Exercicios/profiles/src/styles/themes.js
@@ -1,13 +1,17 @@
 export const lightTheme = {
-  BACKGROUND: '#CBF7ED',
-  HEADER_BG: '#406E8E',
-  PRIMARY: '#23395B',
-  SECONDARY: '#8EA8C3'
+  COLORS: {
+    BACKGROUND: '#CBF7ED',
+    HEADER_BG: '#406E8E',
+    PRIMARY: '#23395B',
+    SECONDARY: '#8EA8C3'
+  }
 }
 
 export const darkTheme = {
-  BACKGROUND: '#1A181B',
-  HEADER_BG: '#000000',
-  PRIMARY: '#706C61',
-  SECONDARY: '#66666E'
+  COLORS: {
+    BACKGROUND: '#1A181B',
+    HEADER_BG: '#000000',
+    PRIMARY: '#706C61',
+    SECONDARY: '#66666E'
+  }
 }
